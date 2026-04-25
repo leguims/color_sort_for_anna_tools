328c76421b389889a17cd1e0a384ed83cf1b6667
leguims <leguims@free.fr>
2026-04-25 15:25:42 +0200

    robustesse import circulaire.

--- core/plateau/__init__.py
+++ core/plateau/__init__.py
@@ -1,2 +1,22 @@
-from .model import Plateau
-from .exceptions import PlateauInvalidable
+"""
+Package plateau : expose uniquement Plateau à l'extérieur,
+tout en évitant les imports circulaires.
+"""
+
+# On importe d'abord les sous-modules pour casser les cycles
+from . import model
+from . import format
+from . import ops
+from . import generator
+from . import validator
+from . import normalize
+from . import heuristics
+from . import exceptions
+
+# Maintenant que tout est chargé, on peut exposer Plateau
+Plateau = model.Plateau
+
+# Et éventuellement l'exception publique
+PlateauInvalidable = exceptions.PlateauInvalidable
+
+__all__ = ["Plateau", "PlateauInvalidable"]
