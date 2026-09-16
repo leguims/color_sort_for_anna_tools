b7d799e28d2062d9d8971e9d478bcce8c67e20b7
leguims <leguims@free.fr>
2026-09-16 20:10:50 +0200

    import JSON avec retentatives pendant 15 mins.

--- io_utils/export_json.py
+++ io_utils/export_json.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 import json
 from pathlib import Path
 
@@ -40,27 +41,27 @@ Retourne True si l'export a ete realise"""
         # Enregistrement des donnees dans un fichier JSON
         if not self._chemin_enregistrement.parent.exists():
             self._chemin_enregistrement.parent.mkdir(parents=True, exist_ok=True)
+        if type(contenu) == dict:
+            contenu_dict = contenu
+        else:
+            try:
+                # Enregistrement d'une classe
+                contenu_dict = contenu.to_dict()
+            except MemoryError as e:
+                print(f"{self.now()} JSON.forcer_export() MemoryError '{self._chemin_court_str}'")
+                # print(f"MemoryError : '{e}'")
+                return False
         try:
-            if type(contenu) == dict:
-                contenu_dict = contenu
-            else:
-                try:
-                    # Enregistrement d'une classe
-                    contenu_dict = contenu.to_dict()
-                except MemoryError as e:
-                    print(f"{self.now()} JSON.forcer_export() MemoryError '{self._chemin_court_str}'")
-                    # print(f"MemoryError : '{e}'")
-                    return False
             with open(self._chemin_enregistrement, "w", encoding='utf-8') as fichier:
                 if 'Resolution' not in str(self._chemin_enregistrement):
                     print(f"{self.now()} JSON.forcer_export() fichier ouvert '{self._chemin_court_str}'")
                 json.dump(contenu_dict, fichier, ensure_ascii=False, indent=4)
-            if 'Resolution' not in str(self._chemin_enregistrement):
-                print(f"{self.now()} JSON.forcer_export() fichier ferme '{self._chemin_court_str}'")
         except OSError as e:
             print(f"{self.now()} JSON.forcer_export() OSError '{self._chemin_court_str}'")
             print(f"OSError : '{e}'")
             return False
+        if 'Resolution' not in str(self._chemin_enregistrement):
+            print(f"{self.now()} JSON.forcer_export() fichier ferme '{self._chemin_court_str}'")
 
         self._longueur_dernier_enregistrement = len(contenu_dict)
         self._timestamp_dernier_enregistrement = datetime.datetime.now().timestamp()
@@ -72,19 +73,40 @@ Retourne True si l'export a ete realise"""
 
     def importer(self):
         """Lit dans un fichier JSON les informations totales ou de la derniere iteration realisee."""
-        try:
-            with open(self._chemin_enregistrement, "r", encoding='utf-8') as fichier:
+        # 5 tentatives de lecture à : 0min, 1min, 3min, 6min, 10min et 15min
+        for attente_en_min in range(1,6): # Range cumulé = 15
+            try:
+                with open(self._chemin_enregistrement, "r", encoding='utf-8') as fichier:
+                    if 'Resolution' not in str(self._chemin_enregistrement):
+                        print(f"{self.now()} JSON.importer() fichier ouvert '{self._chemin_court_str}'")
+                    dico_json = self.json_load(fichier)
                 if 'Resolution' not in str(self._chemin_enregistrement):
-                    print(f"{self.now()} JSON.importer() fichier ouvert '{self._chemin_court_str}'")
-                dico_json = json.load(fichier)
-            if 'Resolution' not in str(self._chemin_enregistrement):
-                print(f"{self.now()} JSON.importer() fichier ferme '{self._chemin_court_str}'")
-            return dico_json
-        except FileNotFoundError as e:
-            if 'Resolution' not in str(self._chemin_enregistrement):
-                print(f"{self.now()} JSON.importer() FileNotFoundError '{self._chemin_court_str}'")
-                # print(f"FileNotFoundError : '{e}'")
-            return {}
+                    print(f"{self.now()} JSON.importer() fichier ferme '{self._chemin_court_str}'")
+                return dico_json
+
+            except FileNotFoundError as e:
+                if 'Resolution' not in str(self._chemin_enregistrement):
+                    print(f"{self.now()} JSON.importer() FileNotFoundError '{self._chemin_court_str}'")
+                    # print(f"FileNotFoundError : '{e}'")
+                return {}
+            except OSError as e:
+                if 'Resolution' not in str(self._chemin_enregistrement):
+                    print(f"{self.now()} JSON.importer() OSError sur open '{self._chemin_court_str}'")
+                    print(f"OSError : '{e}'")
+                # Attente avant la prochaine tentative de lecture du fichier
+                print(f"{self.now()} JSON.importer() OSError : Nouvelle tentative dans {attente_en_min} minutes")
+                time.sleep(attente_en_min * 60)
+            except MemoryError as e:
+                if 'Resolution' not in str(self._chemin_enregistrement):
+                    print(f"{self.now()} JSON.importer() MemoryError sur open '{self._chemin_court_str}'")
+                    # print(f"MemoryError : '{e}'")
+                return {}
+        return {}
+
+    def json_load(self, fichier):
+        """Decode le JSON du fichier."""
+        try:
+            dico_json = json.load(fichier)
         except json.decoder.JSONDecodeError as e:
             if 'Resolution' not in str(self._chemin_enregistrement):
                 print(f"{self.now()} JSON.importer() JSONDecodeError '{self._chemin_court_str}'")
@@ -92,12 +114,12 @@ Retourne True si l'export a ete realise"""
             return {}
         except OSError as e:
             if 'Resolution' not in str(self._chemin_enregistrement):
-                print(f"{self.now()} JSON.importer() OSError '{self._chemin_court_str}'")
-                print(f"OSError : '{e}'")
-            return {}
+                print(f"{self.now()} JSON.importer() OSError sur json.load '{self._chemin_court_str}'")
+                print(f"OSError sur json.load: '{e}'")
+            raise OSError(e) # Remonter l'exception pour relire le fichier plus tard
         except MemoryError as e:
             if 'Resolution' not in str(self._chemin_enregistrement):
-                print(f"{self.now()} JSON.importer() MemoryError '{self._chemin_court_str}'")
+                print(f"{self.now()} JSON.importer() MemoryError sur json.load '{self._chemin_court_str}'")
                 # print(f"MemoryError : '{e}'")
             return {}
-        return {}
+        return dico_json
