efae4eac0c70c3b4012dace56ca163ed0d1d372a
leguims <leguims@free.fr>
2026-09-16 20:02:26 +0200

    MàJ batch archives.

--- Archives/Archive_pipeline_6_Solutions_Unitaires_7z.bat
+++ Archives/Archive_pipeline_6_Solutions_Unitaires_7z.bat
@@ -1,4 +1,60 @@
-set Path=%Path%;C:\Program Files\7-Zip
+@echo off
+setlocal EnableDelayedExpansion
 
-# Créer l'archive sans filtrer les fichiers
-7z a -t7z 7z\Archive_pipeline_6_Solutions_Unitaires_%date:~6,4%-%date:~3,2%-%date:~0,2%.7z ..\..\Pipelines\pipeline_6_solutions_unitaires -xr!.git
+set PATH=%PATH%;C:\Program Files\7-Zip
+set repertoire_source=..\..\Pipelines\pipeline_6_solutions_unitaires\*
+
+set "TEMP=temp"
+set "OUTDIR=7z"
+set "BATCH=1000"
+
+if not exist "%OUTDIR%" mkdir "%OUTDIR%"
+echo if not exist "%OUTDIR%" mkdir "%OUTDIR%"
+
+for /d %%D in (%repertoire_source%) do (
+    echo ########## Traitement sur : %%~D
+    if /I "%%~nxD"==".git" (
+        echo ##########    Repertoire .git ignore
+    ) else (
+        echo ########## Traitement sur : %%~nxD
+
+        REM Pas de solutions unitaires si elles sont en cours de resolution
+        set resolution_achevee=%OUTDIR%\pipeline_6_Plateaux_Difficulte-Archive_compressee-%%~nxD.7z
+        if exist "!resolution_achevee!" (
+            set nom_cible_compressee=pipeline_6_Solutions_Unitaires-Archive_compressee-%%~nxD.7z
+            if not exist "%OUTDIR%\!nom_cible_compressee!" (
+                echo ##########    !nom_cible_compressee!
+
+                REM Prepare un index de lots
+                set /a idx=0
+                set /a next = idx + BATCH
+
+                REM Creer l'archive en filtrant les fichiers
+                for %%F in (%%~D\*.json) do (
+                    set /a mod = idx %% BATCH
+                    if !mod! EQU 0 (
+                        set nom_cible=%TEMP%\pipeline_6_Solutions_Unitaires-Archive-%%~nxD-!idx!.7z
+                        set /a next = idx + BATCH
+                        set nom_cible_suivante=%TEMP%\pipeline_6_Solutions_Unitaires-Archive-%%~nxD-!next!.7z
+                        echo ##########    !nom_cible!
+                    )
+                    REM Enregistrer seulement si l'archive suivante n'existe pas.
+                    if not exist "!nom_cible_suivante!" (
+                        7z a -mx0 -t7z -- !nom_cible! %%~F >nul
+                    )
+                    set /a idx+=1
+                )
+                REM Compresser l'archive (pas trop pour le temps d'execution)
+                7z a -mx5 -t7z -sdel -- !nom_cible_compressee! %TEMP%\pipeline_6_Solutions_Unitaires-Archive-%%~nxD-*.7z >nul
+                REM Ranger l'archive
+                MOVE !nom_cible_compressee! %OUTDIR%\!nom_cible_compressee!
+            ) else (
+                echo ##########    %%~nxD : deja traite
+            )
+        ) else (
+            echo ##########    %%~nxD : resolution en cours
+        )
+    )
+)
+
+endlocal
---
+++ Archives/Archive_pipeline_8_Solutions_godot_7z.bat
@@ -0,0 +1,12 @@
+@echo off
+set Path=%Path%;C:\Program Files\7-Zip
+set repertoire_source=..\..\Pipelines\pipeline_6_solutions\*.json
+set nom_cible=pipeline_8_Solutions_godot-Archive-%date:~6,4%-%date:~3,2%-%date:~0,2%.7z
+set nom_cible_compressee=pipeline_8_Solutions_godot-Archive_compressee-%date:~6,4%-%date:~3,2%-%date:~0,2%.7z
+
+REM Creer l'archive en filtrant les fichiers
+7z a -mx0 -t7z -xr!.git -xr!.gitignore -- %nom_cible% %repertoire_source%
+REM Compresser l'archive (pas trop pour le temps)
+7z a -mx5 -t7z -sdel -- %nom_cible_compressee% %nom_cible%
+REM Ranger l'archive
+MOVE %nom_cible_compressee% 7z\%nom_cible_compressee%
